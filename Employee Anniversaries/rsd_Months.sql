5768ce8e226c064903badd15519aa0f5d55dced5
Lynn Curtis <lynnettacurtis@gmail.com>
2021-05-12 02:46:23 -0500

    Revised documentation.

--- Employee Anniversaries/rsd_Months.sql
+++ Employee Anniversaries/rsd_Months.sql
@@ -1,3 +1,24 @@
-SELECT DISTINCT MONTH_ID, MONTH_NAME
-FROM     v_Employee_StartDates
-ORDER BY MONTH_ID
+/*
+Author:			
+	Lynn Curtis
+Report:			
+	Anniversary Letters.rdl
+Date:			
+	Jan 2021
+Purpose:		
+	Populates Monthly Selection Dropdown Box
+Design Tool:	
+	SSRS/SSDT
+DBMS:			
+	SQL SERVER 2017, Developer Edition
+DB:				
+	AdventureWorks 2017
+Retention:		
+	KP PERM
+*/
+
+SELECT DISTINCT 
+MONTH_ID, 
+MONTH_NAME
+FROM  v_Employee_StartDates
+ORDER BY MONTH_ID;

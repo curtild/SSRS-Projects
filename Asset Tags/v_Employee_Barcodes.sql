49b216c920cfc8b5afecdc3755483733a47c0d20
Lynn Curtis <lynnettacurtis@gmail.com>
2021-05-13 15:43:48 -0500

    Add files to repo.

---
+++ Asset Tags/v_Employee_Barcodes.sql
@@ -0,0 +1,38 @@
+/*
+Author:			
+	Lynnetta Curtis
+Report:			
+	AssetTag Barcode.rdl
+Created:			
+	Jan 2021
+Purpose:		
+	To create a view of employee data with related barcodes.
+Design Tool:	
+	SSRS/Visual Studio 2019 Community Edition
+DBMS:			
+	SQL SERVER 2017, Developer Edition
+DB:				
+	AdventureWorks 2017
+Dependencies:
+	[TABLE] dbo.Employee_Barcodes (Generated from CreateEmployeeBarcodes.sql)
+Retention:		
+	KP PERM
+*/
+
+IF OBJECT_ID(N'dbo.v_Employee_Barcodes', N'V') IS NOT NULL
+	DROP VIEW dbo.v_Employee_Barcodes;
+GO
+CREATE VIEW v_Employee_Barcodes 
+AS
+(SELECT RECORD_NO, 
+FIRSTNAME, 
+MIDDLENAME, 
+LASTNAME, 
+EMPLOYEE, 
+JOBTITLE, 
+DEPARTMENT, 
+PHONENUMBER, 
+EMAILADDRESS, 
+BARCODE_TEXT, 
+BARCODE_IMAGE
+FROM dbo.Employee_Barcodes)
